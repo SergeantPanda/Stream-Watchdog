4c1b050d3ba6f1e31e371891c316dbc8c179a5ae
SergeantPanda <chester.cullen@gmail.com>
2025-01-21 08:26:44 -0600

    Updated license.

--- README.md
+++ README.md
@@ -114,7 +114,7 @@ Buffering resolved on channel 103 - News Channel.
 Contributions are welcome! Feel free to submit issues or pull requests to improve this script.
 
 ## License
-This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+This project is licensed under the GNU General Public License v3.0. See the [LICENSE](./LICENSE) file for details.
 
 ## Acknowledgments
 Special thanks to the Stream Master developer Senex for providing a robust API to enhance streaming experiences.
--- Stream-Master-Watchdog.py
+++ Stream-Master-Watchdog.py
@@ -1,3 +1,20 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This file is part of Stream Master Watchdog.
+#
+# Stream Master Watchdog is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Stream Master Watchdog is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Stream Master Watchdog. If not, see <https://www.gnu.org/licenses/>.
+
+
 import subprocess
 import requests
 import re
