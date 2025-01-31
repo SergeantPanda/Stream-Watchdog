f27524d64fc3674d5aa9ba73e6e70308bb1b9cb2
SergeantPanda <chester.cullen@gmail.com>
2025-01-30 20:21:20 -0600

    Added support for authentication.

--- Modules/Stream_Master.py
+++ Modules/Stream_Master.py
@@ -16,15 +16,33 @@
 
 import requests
 
+session = None
+
 def stream_url_template(SERVER_URL):
     return f"{SERVER_URL}/v/0/{{id}}"
 
-def get_running_streams(stream_master_url):
-    """Fetch current running streams from the API."""
+def login(stream_master_url, USERNAME, PASSWORD):
+    global session
+    if session is None:
+        session = requests.Session()
+
+        # Define login URL and credentials
+        login_url = f"{stream_master_url}/login"
+        credentials = {"username": USERNAME, "password": PASSWORD}
+
+        # Perform the login
+        session.post(login_url, data=credentials)
+    return session
+
+def get_running_streams(stream_master_url, USERNAME = None, PASSWORD= None):
+    """Fetch current running streams from the API."""   
     watchdog_names = {}  # Store stream names
+    headers = {"Accept": "application/json"}
     try:
         CHANNEL_METRICS_API_URL = f"{stream_master_url}/api/statistics/getchannelmetrics"
-        response = requests.get(CHANNEL_METRICS_API_URL, headers={"accept": "application/json"})
+        if USERNAME is not None:
+            session = login(stream_master_url, USERNAME, PASSWORD)
+        response = session.get(CHANNEL_METRICS_API_URL, headers=headers)
         response.raise_for_status()
         streams = response.json()
         
@@ -50,13 +68,15 @@ def get_running_streams(stream_master_url):
         print(f"Error fetching streams: {e}")
         return [], {}  # Return empty structures on failure
     
-def send_next_stream(stream_id, stream_master_url):
+def send_next_stream(stream_id, stream_master_url, USERNAME = None, PASSWORD = None):
     """Handle the buffering event by switching to the next stream."""
     try:
         NEXT_STREAM_API_URL = f"{stream_master_url}/api/streaming/movetonextstream"
         # Trigger the next stream switch
+        if USERNAME is not None:
+            session = login(stream_master_url, USERNAME, PASSWORD)
         payload = {"SMChannelId": stream_id}
-        response = requests.patch(
+        response = session.patch(
             NEXT_STREAM_API_URL,
             json=payload,
             headers={
